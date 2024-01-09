34205206062c27d63ef0dae1c46df936a17eea55
ri-naa <rina288@ewhain.net>
2024-01-10 02:02:39 +0900

    [feat] 재난문자 api 테스트 #3

---
+++ src/main/java/com/ALGo/ALGo_server/message/dto/MessageResponse.java
@@ -0,0 +1,23 @@
+package com.ALGo.ALGo_server.message.dto;
+
+import lombok.Data;
+
+@Data
+public class MessageResponse {
+    private String DSSTR_SE_NM;
+    private String CREAT_DT;
+    private String RCV_AREA_NM;
+    private String MD101_SN;
+    private String DSSTR_SE_ID;
+    private String MODF_DT;
+    private String RCV_AREA_ID;
+    private String UPDUSR_ID;
+    private String MSG_SE_CD;
+    private String DELETE_AT;
+    private String DT_REGT;
+    private String MSG_CN;
+    private String EMRGNCY_STEP_ID;
+    private String REGIST_DT;
+    private String REGISTER_ID;
+    private String EMRGNCY_STEP_NM;
+}
