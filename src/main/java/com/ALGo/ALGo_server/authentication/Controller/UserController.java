2eaaa61120f46c1fd0687ad47ab7100a5e304146
yunji118 <yunji118@naver.com>
2024-01-10 19:45:54 +0900

    [feat] 회원가입 #9

--- src/main/java/com/ALGo/ALGo_server/authentication/Controller/UserController.java
+++ src/main/java/com/ALGo/ALGo_server/authentication/Controller/UserController.java
@@ -1,2 +1,25 @@
-package com.ALGo.ALGo_server.authentication.Controller;public class UserController {
+package com.ALGo.ALGo_server.authentication.Controller;
+
+import com.ALGo.ALGo_server.authentication.Dto.JoinRequestDto;
+import com.ALGo.ALGo_server.authentication.Jwt.JwtTokenProvider;
+import com.ALGo.ALGo_server.authentication.Service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @PostMapping("/join")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity join(@RequestBody JoinRequestDto requestDto){
+        userService.join(requestDto);
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
 }
