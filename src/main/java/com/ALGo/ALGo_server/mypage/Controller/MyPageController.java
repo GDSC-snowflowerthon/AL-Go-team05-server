d13740866880856061b0d7cc561898aa3b401d22
yunji118 <yunji118@naver.com>
2024-01-11 19:56:58 +0900

    [fix] 언어/지역 변경 url 수정 #22

--- src/main/java/com/ALGo/ALGo_server/mypage/Controller/MyPageController.java
+++ src/main/java/com/ALGo/ALGo_server/mypage/Controller/MyPageController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/mypage")
+@RequestMapping("/setting")
 public class MyPageController {
     private final MyPageService myPageService;
 
