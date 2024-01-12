b5a90b6519820e0239bfa9ce40f723a6a062fac6
yunji118 <yunji118@naver.com>
2024-01-12 19:37:40 +0900

    [feat] quiz 중복제거 #32

--- src/main/java/com/ALGo/ALGo_server/quiz/Service/QuizService.java
+++ src/main/java/com/ALGo/ALGo_server/quiz/Service/QuizService.java
@@ -9,9 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 @Service
 @RequiredArgsConstructor
@@ -22,14 +20,14 @@ public class QuizService {
     public QuizResponseDto getQuiz(){
         Random random = new Random();
 
-        List<QuizDto> quizDtoList = new ArrayList<>();
-        for(int i=0;i<4;i++){
+        Set<QuizDto> quizDtoList = new HashSet<>();
+        while (quizDtoList.size() < 4){
             Long randomNum = Long.valueOf(random.nextInt(16) + 1);
             Quiz q = quizRepository.findById(randomNum).get();
             QuizDto quizDto = new QuizDto(q);
             quizDtoList.add(quizDto);
         }
-        QuizResponseDto responseDto = new QuizResponseDto(quizDtoList);
+        QuizResponseDto responseDto = new QuizResponseDto(quizDtoList.stream().toList());
 
         return responseDto;
     }
